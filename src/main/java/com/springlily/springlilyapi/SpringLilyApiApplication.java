e0857cdd50e9f203f57facf20c220b95304b697a
sal <sal2993@hotmail.com>
2018-11-04 10:16:57 -0800

    spring initializr app

---
+++ src/main/java/com/springlily/springlilyapi/SpringLilyApiApplication.java
@@ -0,0 +1,12 @@
+package com.springlily.springlilyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLilyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLilyApiApplication.class, args);
+	}
+}
